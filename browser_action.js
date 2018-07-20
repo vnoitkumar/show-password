b2957694a28af7c15f58bf9d172357b3863964cf
Vinoth Kumar S <svinoth@finmomenta.com>
2018-07-20 11:26:16 +0530

    Working on the toggle icon

--- browser_action.js
+++ browser_action.js
@@ -1,3 +1,20 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(null, {file: "show_password.min.js"});
+    toggle = !toggle;
+    console.log('onClicked');
+    if (toggle) {
+        chrome.browserAction.setIcon({
+            path: "icon-16X16.png" // Show
+        });
+        chrome.tabs.executeScript(null, {
+            file: "show_password.js"
+        });
+    } else {
+        chrome.browserAction.setIcon({
+            path: "icon-16X16.png" // Hide
+        });
+        chrome.tabs.executeScript(null, {
+            file: "hide_password.js"
+        });
+    }
+
 });
---
+++ hide_password.js
@@ -0,0 +1,12 @@
+var inputDomElements = document.getElementsByTagName('input');
+var passwordInputDomElements = document.getElementsByClassName('show-password-by-vnoit-show-password-extension');
+
+
+for (var i = 0; i < passwordInputDomElements.length; i++) {
+    var passwordDomElement = passwordInputDomElements[i];
+    if (passwordDomElement.getAttribute('type').toLowerCase() === 'text') {
+        passwordDomElement.classList.remove("show-password-by-vnoit-show-password-extension");
+        passwordDomElement.type = 'password';
+    }
+}
+
